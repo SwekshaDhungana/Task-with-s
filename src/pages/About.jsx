33721fe59444e769e899d0a4761534f0f4608049
Your Name <you@example.com>
2025-06-15 22:52:14 +0545

    changed about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function About() {
-  return <div className="sales-container">Welcome to About</div>;
+  return (
+    <div className="sales-container">
+      <h1>This is about page</h1>
+    </div>
+  );
 }
 
 export default About;
